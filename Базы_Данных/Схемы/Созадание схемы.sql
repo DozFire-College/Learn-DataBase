1285d1661fada549d9a364018375e7f326887c81
DozFire-College <ttylllihka@gmail.com>
2026-09-04 11:48:47 +0300

    03.09.2026. Sheme and Functions

---
+++ Базы_Данных/Схемы/Созадание схемы.sql
@@ -0,0 +1,11 @@
+USE DB1
+Go
+
+--Create SCHEMA department
+--Go
+--Create SCHEMA customers
+--GO
+--CREATE TABLE department.person(id int, name varchar(100))
+
+ALTER SCHEMA customers
+	TRANSFER department.person 
---
+++ Базы_Данных/Функции/СлучайныйРезультат.sql
@@ -0,0 +1,17 @@
+USE DB2
+Go
+
+--SELECT first_name, RAND(age)
+--FROM users
+--SELECT first_name, RAND(age)
+--FROM users
+--SELECT first_name, age
+--FROM users
+--ORDER BY age ASC
+--Go
+--SELECT first_name, age
+--FROM users
+--ORDER BY first_name ASC
+
+SELECT COUNT(*)
+FROM users
